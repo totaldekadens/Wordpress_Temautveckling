160ef8dc7545253e87b80345fae340222acf07b7
Angelica Moberg <angelica.moberg@hotmail.com>
2022-04-08 17:22:16 +0200

    wops

--- commonLoop.php
+++ commonLoop.php
@@ -5,7 +5,7 @@
 		<div class="container">
 			<div class="row">
 				<div id="primary" class="col-xs-12 col-md-9">
-					<h1><?php the_author(); ?></h1>
+					<h1><?php wp_title(''); ?></h1>
 					<?php 
 					while (have_posts()) {
 						the_post();
