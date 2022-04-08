ea90f1dbb27d0a0a97da424188ff06f2e95f8961
Angelica Moberg <angelica.moberg@hotmail.com>
2022-04-08 14:55:28 +0200

    Pagination fixad!

--- template-parts/pagination.php
+++ template-parts/pagination.php
@@ -1,9 +1,12 @@
 
 <!-- Gör en pagination -->
-<nav class="navigation pagination">
-    <h2 class="screen-reader-text">Inläggsnavigering</h2>
-    <a class="prev page-numbers" href="">Föregående</a>
-    <span class="page-numbers current">1</span>
-    <a class="page-numbers" href="">2</a>
-    <a class="next page-numbers" href="">Nästa</a>
-</nav>
+
+<?php 
+the_posts_pagination( array(
+    'mid_size'  => 2,
+    'prev_text' => __( 'Föregående'),
+    'next_text' => __( 'Nästa' ),
+    'class' => 'pagination',
+) ); 
+?>
+
