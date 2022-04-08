7bc23907e1a5c70072c390d7db1e73de64d0a9a3
Angelica Moberg <angelica.moberg@hotmail.com>
2022-04-08 08:45:27 +0200

    Frontpage skapad

---
+++ front-page.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="hero">
+                    <?php
+                    while (have_posts()) {
+                        the_post(); ?>
+                        <?php the_post_thumbnail(null, ['class' => 'img-responsive responsive--full', 'title' => 'Feature image'] );?>
+                        <div class="text">
+                            <h1><?php the_title(); ?></h1>
+                            <p><?php the_content(); ?></p>
+                        </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
